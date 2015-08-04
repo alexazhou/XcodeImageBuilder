a209afb8ef554d09f4a41cb9950170444922f2bf
AlexaZhou <AlexaZhou@163.com>
2015-08-05 00:10:05 +0800

    加入spider模块

---
+++ spider.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2015-08-04 23:44:38
+# @Author  : Alexa (AlexaZhou@163.com)
+# @Link    : 
+# @Disc    : 
+import resize
+
+input_base_dir = "ImageBuilderResources"
+output_base_dir = "XcodeImageOutputs"
+
+def process_dir(input_path, output_path):
+	pass
+
+
+def main():
+	process_dir( input_base_dir, output_base_dir )
+
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print ('spider.py had been imported as a module')
