8718a52825a53e00deaa6369acd19a44ccbd8887
AlexaZhou <AlexaZhou@163.com>
2015-08-05 14:20:05 +0800

    通过spider对图片进行遍历

--- spider.py
+++ spider.py
@@ -4,19 +4,32 @@
 # @Author  : Alexa (AlexaZhou@163.com)
 # @Link    : 
 # @Disc    : 
+
+import os
+import os.path
 import resize
 
 input_base_dir = "ImageBuilderResources"
 output_base_dir = "XcodeImageOutputs"
 
 def process_dir(input_path, output_path):
-	pass
+	
+    for parent,dirnames,filenames in os.walk(input_path):
+
+        output_parent = output_base_dir + parent[len(input_base_dir):]
+
+        if os.path.exists(output_parent) == False:
+            os.mkdir(output_parent)
+
+        for f in filenames:
+            if f.startswith("."):
+                continue
+
+            resize.resize_img( parent+os.sep+f, output_parent+os.sep+f )
 
 
 def main():
-	process_dir( input_base_dir, output_base_dir )
-
-
+    process_dir( input_base_dir, output_base_dir )
 
 if __name__ == '__main__':
     main()
